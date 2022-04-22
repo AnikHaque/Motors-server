42c8392da301c24ab12252d1c9e48ae8e5c6dfb0
AnikHaque <ekramul15-811@diu.edu.bd>
2022-04-23 04:09:10 +0600

    updated payment gateway

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const app = express()
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 5000;
 
 // middleware 
