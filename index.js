0dd00f8d3642be58d9b7dcec8148b11111bb0d2e
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-23 13:01:05 -0500

    updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+// hello 
 async function run() {
     try {
       await client.connect();
@@ -21,7 +21,7 @@ async function run() {
        const carscollection = database.collection("cars");
        const specialcollection = database.collection("special");
         const bookingcollection = database.collection("booking");
-    // // //   const reservecollection = database.collection("reserve");
+ 
          const userscollection = database.collection("users");
          const reviewCollection = database.collection("reviews");
    
