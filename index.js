2078a1d293c870d0d9763683827a250d276fc9e5
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-23 13:16:53 -0500

    deleted comments

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// hello 
+
 async function run() {
     try {
       await client.connect();
