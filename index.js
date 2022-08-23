94bd874175a42b8c7035158fadb429d4e1d63ce7
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-23 13:13:18 -0500

    added

--- index.js
+++ index.js
@@ -113,15 +113,6 @@ app.post('/special', async(req, res) => {
           
   })
 
-
-
-
-
-
-
-
-        
-
   // POST API TO ADD BOOKING OF ANY ROOM 
 app.post('/booking', async(req, res) => {
   const newroom = req.body; 
